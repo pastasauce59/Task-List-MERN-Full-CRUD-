308766ec9e50431be169f66b70ea727df14b2765
Michael Primavera <pastasauce59@gmail.com>
2024-06-08 10:32:18 -0400

    Created package.json file and Server.js file

---
+++ backend/Server.js
@@ -0,0 +1,18 @@
+const express =  require("express")
+
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const cors = require("cors")
+
+const app = express()
+const PORT = process.env.PORT | 5000
+
+app.arguments(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("HELLO WORLD!!!")
+})
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}`))
